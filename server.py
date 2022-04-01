b923b3e45730d978737d43d0723550c535a8176d
EtienneFerrandi <etienne.ferrandi@hotmail.fr>
2022-04-01 11:54:42 +0200

    modification

--- server.py
+++ server.py
@@ -3,24 +3,35 @@ import socket
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.bind(('', 8080))
 
+socket.listen(5)
+
+client1, adress1 = socket.accept()
+print("{} connected".format(adress1))
+
+client1.sendall(("j1"+","+"x").encode())
+
+client2, adress2 = socket.accept()
+print("{} connected".format(adress2))
+
+client2.sendall(("j2"+","+"o").encode())
+
+client1.sendall("start".encode())
+client2.sendall("start".encode())
+
 while True:
-        socket.listen(5)
-        client, adress = socket.accept()
-        client2, adress2 = socket.accept()
-
-        print("{} connected".format(adress))
-        print("{} connected".format(adress2))
-
-        response = client.recv(255)
+        response = client1.recv(255)
         response2 = client2.recv(255)
         if response != "":
                 print(response)
-        client.sendall(response)
+        client1.sendall(response)
         if response2 != "":
                 print(response2)
         client2.sendall(response2)
         client2.sendall(response)
-        client.sendall(response2)
+        client1.sendall(response2)
+
+
+
 print ("Close")
 client.close()
 stock.close()
